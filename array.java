2b28dac7fbf1717603ebd8defa42d4b4980c8b1d
nayanavnair01 <nayananair2006@gmail.com>
2025-08-13 09:29:32 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class array {
+	public static void main(String [] args) {
+		int [] array = {10,20,30};
+		System.out.println(array.length);
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]+"\t");
+		}
+	}
+
+}
